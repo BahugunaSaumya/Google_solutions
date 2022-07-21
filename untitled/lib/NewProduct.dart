716a60bfb6d9dfc5739949db495cad438d9dc80a
Kinjal-16 <bhattacharyyakinjal@gmail.com>
2022-07-21 20:58:49 +0530

    New Product page(in progress)

--- untitled/lib/NewProduct.dart
+++ untitled/lib/NewProduct.dart
@@ -7,6 +7,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:dropdown_button2/dropdown_button2.dart';
 
+import 'firebase_options.dart';
 import 'logincustomer.dart';
 import 'loginfarmer.dart';
 
@@ -15,12 +16,22 @@ void main() {
 }
 
 class new_product extends StatelessWidget {
+  static var user_id;
+
+
+  static var de;
+  static String se;
+  static List<DocumentSnapshot> it;
   const new_product({Key key}) : super(key: key);
 
   static const String _title = 'New product';
 
   @override
   Widget build(BuildContext context) {
+    MyStatelessWidget.user_id=user_id;
+    MyStatelessWidget.items = it;
+    MyStatelessWidget.decide = de;
+    MyStatelessWidget.selectedValue = se;
     return MaterialApp(
       title: _title,
       home: Scaffold(
@@ -47,49 +58,146 @@ class new_product extends StatelessWidget {
 
 class MyStatelessWidget extends StatelessWidget {
   MyStatelessWidget({Key key}) : super(key: key);
-
-
-  var selectedValue = "";
-  static List<DocumentSnapshot> documents;
-  static List<String> categories=new List();
+  static var user_id;
+  static String selectedValue;
+  var changedvalue;
+  static var decide;
+  static int pass;
+  static List<String> categories = new List();
   final _formKey = GlobalKey<FormState>();
 
+  static List<DocumentSnapshot> documents;
+  static List<DocumentSnapshot> items;
+
   Future<void> readExcelFile() async {
-    final result =
-    await FirebaseFirestore.instance.collection('Categories').get();
+    await Firebase.initializeApp(
+        options: DefaultFirebaseOptions.currentPlatform);
+    FirebaseFirestore db = FirebaseFirestore.instance;
+    print("hello");
+    if (selectedValue == null) {
+      selectedValue =
+          "Select value"; // change value for comapre to function as well on changing this value line- 168 and 233
+    }
+    if (decide != false) {
+      decide = true;
+    }
+
+
+    final result = await db.collection('Categories').get();
 
     documents = result.docs;
-    for (int i = 0; i < documents.length; i++) {
-      categories.add(documents[i].id);
+    if (items == null) {
+      items = [];
     }
-    print(documents.length);
+    print(decide);
+    print(selectedValue);
+  }
+
+  Future<void> getitem() async {
+    await Firebase.initializeApp(
+        options: DefaultFirebaseOptions.currentPlatform);
+    FirebaseFirestore db = FirebaseFirestore.instance;
+    print("hello");
+
+
+
+    final result = await db
+        .collection('Categories')
+        .doc(selectedValue)
+        .collection("Items")
+        .get();
+
+    items = result.docs;
+
+    print(items.length);
   }
 
   @override
   Widget build(BuildContext context) {
+    TextEditingController qty = TextEditingController();
+
+    print(categories);
     return Scaffold(
         body: Form(
             key: _formKey,
             child: FutureBuilder<void>(
                 future: readExcelFile(),
                 // a previously-obtained Future<String> or null
-                builder: (BuildContext context,
-                    AsyncSnapshot<void> snapshot) {
+                builder: (BuildContext context, AsyncSnapshot<void> snapshot) {
                   List<Widget> children;
-                  if (categories!=null) {
+                  if (documents != null) {
                     children = <Widget>[
-                      const Icon(
-                        Icons.check_circle_outline,
-                        color: Colors.green,
-                        size: 60,
-                      ),
-                      Padding(
-                          padding: const EdgeInsets.symmetric(horizontal: 80),
-                          child: Column(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                const SizedBox(height: 30),
-                                DropdownButtonFormField2(
+                      Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            const SizedBox(height: 30),
+                            DropdownButtonFormField2(
+                              decoration: InputDecoration(
+                                //Add isDense true and zero Padding.
+                                //Add Horizontal padding using buttonPadding and Vertical padding by increasing buttonHeight instead of add Padding here so that The whole TextField Button become clickable, and also the dropdown menu open under The whole TextField Button.
+                                isDense: true,
+                                contentPadding: EdgeInsets.zero,
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(15),
+                                ),
+                                //Add more decoration as you want here
+
+                                //Add label If you want but add hint outside the decoration to be aligned in the button perfectly.
+                              ),
+                              isExpanded: true,
+                              hint: Text(
+                                selectedValue,
+                                style: TextStyle(fontSize: 14),
+                              ),
+                              icon: const Icon(
+                                Icons.arrow_drop_down,
+                                color: Colors.black45,
+                              ),
+                              iconSize: 30,
+                              buttonHeight: 60,
+                              buttonPadding:
+                                  const EdgeInsets.only(left: 20, right: 10),
+                              dropdownDecoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(15),
+                              ),
+                              items: documents
+                                  .map((item) => DropdownMenuItem<String>(
+                                        value: item.id,
+                                        child: Text(
+                                          item.id,
+                                          style: const TextStyle(
+                                            fontSize: 14,
+                                          ),
+                                        ),
+                                      ))
+                                  .toList(),
+                              validator: (value) {
+                                if (value == null &&
+                                    selectedValue.compareTo("Select value") ==
+                                        0) {
+                                  return 'Please select gender.';
+                                }
+                              },
+                              onChanged: (value) {
+                                //Do something when changing the item if you want.
+                                selectedValue = value.toString();
+                                //  decide = false;
+                                print(selectedValue);
+                                new_product.de = false;
+                                getitem();
+                                new_product.it = items;
+                                new_product.se = selectedValue;
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) => new_product()),
+                                );
+                              },
+                              onSaved: (changedvalue) {},
+                            ),
+                            IgnorePointer(
+                                ignoring: decide,
+                                child: DropdownButtonFormField2(
                                   decoration: InputDecoration(
                                     //Add isDense true and zero Padding.
                                     //Add Horizontal padding using buttonPadding and Vertical padding by increasing buttonHeight instead of add Padding here so that The whole TextField Button become clickable, and also the dropdown menu open under The whole TextField Button.
@@ -103,7 +211,7 @@ class MyStatelessWidget extends StatelessWidget {
                                   ),
                                   isExpanded: true,
                                   hint: const Text(
-                                    'Select Your Gender',
+                                    'Select Your Item',
                                     style: TextStyle(fontSize: 14),
                                   ),
                                   icon: const Icon(
@@ -117,36 +225,75 @@ class MyStatelessWidget extends StatelessWidget {
                                   dropdownDecoration: BoxDecoration(
                                     borderRadius: BorderRadius.circular(15),
                                   ),
-                                  items: categories
-                                      .map((item) =>
-                                      DropdownMenuItem<String>(
-                                        value: item,
-                                        child: Text(
-                                          item,
-                                          style: const TextStyle(
-                                            fontSize: 14,
-                                          ),
-                                        ),
-                                      ))
+                                  items: items
+                                      .map((item) => DropdownMenuItem<String>(
+                                            value: item.id,
+                                            child: Text(
+                                              item.id,
+                                              style: TextStyle(
+                                                fontSize: 14,
+                                                color:
+                                                    decide ? Colors.grey : null,
+                                              ),
+                                            ),
+                                          ))
                                       .toList(),
                                   validator: (value) {
-                                    if (value == null) {
+                                    if (value == null &&
+                                        selectedValue
+                                                .compareTo("Select value") ==
+                                            0) {
                                       return 'Please select gender.';
                                     }
                                   },
                                   onChanged: (value) {
-                                    //Do something when changing the item if you want.
+                                    changedvalue = value.toString();
+                                    print(changedvalue);
                                   },
-                                  onSaved: (value) {
-                                    selectedValue = value.toString();
-                                  },
+                                  onSaved: (value) {},
+                                )),
+                            const SizedBox(height: 50),
+
+                            TextFormField(
+                                controller:qty,
+                                decoration: const InputDecoration(
+                                  icon: Icon(Icons.add),
+
+                                  hintText: 'Enter the amount produced',
+                                  labelText: 'Qty *',
                                 ),
-                              ]
-                          )
-                      )
+                            ),
+
+                            const SizedBox(height: 100),
+                            TextButton(
+                              onPressed: () async {
+                                print(user_id);
+
+                                FirebaseFirestore db = FirebaseFirestore.instance;
+                                final userDocRef = db.collection("Farmer's Item").doc(user_id).collection(selectedValue).doc(changedvalue);
+
+
+
+                                userDocRef.get().then(
+                                      (DocumentSnapshot doc) {
+                                    final data = doc.data() as Map<String, dynamic>;
+                                    pass=data["Qty"];
+
+                                  },
+                                  onError: (e) => print("Error getting document: $e"),
+                                );
+                                final Qty=<String, int>{
+                                  "Qty":(5),
+                                };
+                                userDocRef.set(Qty)
+                                    .onError((e, _) => print("Error writing document: $e"));
+                                }
+                              ,
+                              child: const Text('Submit'),
+                            ),
+                          ])
                     ];
-                  }
-                 else {
+                  } else {
                     children = const <Widget>[
                       SizedBox(
                         width: 60,
@@ -159,14 +306,14 @@ class MyStatelessWidget extends StatelessWidget {
                       )
                     ];
                   }
+
                   return Center(
                     child: Column(
                       mainAxisAlignment: MainAxisAlignment.center,
                       children: children,
                     ),
                   );
-                }
-            )
-        ));
+                })));
   }
 }
+
